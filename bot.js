4d44ae70d7b777b4a956ced580666c5e00b7c6e2
yaebalnunahui-design <yaebalnunahui@gmail.com>
2026-04-16 18:07:45 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,19 +2,10 @@ const TelegramBot = require("node-telegram-bot-api");
 
 const token = "8589160707:AAEHCqUhzfom1D3_gtlG5eTiIrtPnXCGnNk";
 
-console.log("START FILE");
-
-const bot = new TelegramBot(token, {
-  polling: {
-    interval: 1000,
-    autoStart: true
-  }
-});
+const bot = new TelegramBot(token, { polling: true });
 
 bot.on("message", (msg) => {
-  console.log("MESSAGE:", msg.text);
-
   bot.sendMessage(msg.chat.id, "работаю ✅");
 });
 
-console.log("BOT LOADED");
+console.log("Bot started");
