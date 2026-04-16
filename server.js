3ddd512f07a68f3d598d0cea3528740df239181f
yaebalnunahui-design <yaebalnunahui@gmail.com>
2026-04-16 23:59:00 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,1 @@
-const express = require("express");
-const app = express();
 
-app.use(express.json());
-
-// проверка что сервер жив
-app.get("/", (req, res) => {
-  res.send("server ok");
-});
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Server started");
-});
-
-// запускаем бот внутри сервера
-require("./bot.js");
