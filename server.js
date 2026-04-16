2369a108a362d5bfbb934d8e75a3577c2e62bc57
yaebalnunahui-design <yaebalnunahui@gmail.com>
2026-04-16 18:19:56 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,38 +3,8 @@ const app = express();
 
 app.use(express.json());
 
-// хранилище в памяти (пока простое)
-let banned = [];
-let allowed = [];
-
-// проверка бана
-app.get("/check/:id", (req, res) => {
-  res.json({ banned: banned.includes(req.params.id) });
-});
-
-// бан
-app.post("/ban", (req, res) => {
-  const { id } = req.body;
-  if (!banned.includes(id)) banned.push(id);
-  res.json({ ok: true });
-});
-
-// разбан
-app.post("/unban", (req, res) => {
-  const { id } = req.body;
-  banned = banned.filter(x => x !== id);
-  res.json({ ok: true });
-});
-
-// разрешить дальше
-app.post("/allow", (req, res) => {
-  const { id } = req.body;
-  if (!allowed.includes(id)) allowed.push(id);
-  res.json({ ok: true });
-});
-
 app.get("/", (req, res) => {
-  res.send("Server running");
+  res.send("server ok");
 });
 
 app.listen(process.env.PORT || 3000, () => {
