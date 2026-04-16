2dbec3dbcb975bc13b5693c6f91145b23b7d644a
yaebalnunahui-design <yaebalnunahui@gmail.com>
2026-04-16 17:30:46 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,27 +4,10 @@ const token = "8589160707:AAEHCqUhzfom1D3_gtlG5eTiIrtPnXCGnNk";
 
 const bot = new TelegramBot(token, { polling: true });
 
-// тест команды
-bot.onText(/\/test/, (msg) => {
-  bot.sendMessage(msg.chat.id, "🆕 Новый Лог", {
-    reply_markup: {
-      inline_keyboard: [
-        [
-          { text: "🚫 Бан", callback_data: "ban_123" },
-          { text: "✅ Разбан", callback_data: "unban_123" }
-        ],
-        [
-          { text: "➡️ СМС/КОД", callback_data: "allow_123" }
-        ]
-      ]
-    }
-  });
-});
+bot.on("message", (msg) => {
+  console.log("MSG:", msg.text);
 
-// кнопки (пока просто лог)
-bot.on("callback_query", (q) => {
-  console.log("CLICK:", q.data);
-  bot.answerCallbackQuery(q.id);
+  bot.sendMessage(msg.chat.id, "Я вижу сообщение ✅: " + msg.text);
 });
 
 console.log("Bot started");
